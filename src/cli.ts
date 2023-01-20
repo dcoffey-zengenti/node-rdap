9c0adaa49b64c8c5ad09919ec6fa7c869f516c65
Derry Coffey <d.coffey@zengenti.com>
2023-01-20 12:36:02 +0000

    fix: Replaced yargs with commander. Made an interactive CLI shell.

--- src/cli.ts
+++ src/cli.ts
@@ -1,46 +1,61 @@
 import { autnum, domain, ip } from "./client";
-import yargs from "yargs";
-import { hideBin } from "yargs/helpers";
+import { program, InvalidArgumentError } from "commander";
+import * as readline from "readline";
 
-yargs(hideBin(process.argv))
-  .command(
-    "domain <domain>",
-    "get the rdap response for a domain",
-    {
-      domain: {
-        type: "string",
-        demandOption: true,
-      },
-    },
-    async (argv) => {
-      console.log(await domain(argv.domain));
-    }
-  )
-  .command(
-    "ip <ip>",
-    "get the rdap response for a ip",
-    {
-      ip: {
-        type: "string",
-        demandOption: true,
-      },
-    },
-    async (argv) => {
-      console.log(await ip(argv.ip));
-    }
-  )
-  .command(
-    "autnum <autnum>",
-    "get the rdap response for a autnum",
-    {
-      autnum: {
-        type: "number",
-        demandOption: true,
-      },
-    },
-    async (argv) => {
-      console.log(await autnum(argv.autnum));
-    }
-  )
-  .demandCommand(1)
-  .parse();
+const doCommand = async <T>(fn: (arg: T) => Promise<any>, arg: T) => {
+  try {
+    console.log(await fn(arg));
+  } catch (e) {
+    console.error((e as Error).toString());
+  }
+};
+
+const parseArgInt = (value) => {
+  // parseInt takes a string and a radix
+  const parsedValue = parseInt(value, 10);
+  if (isNaN(parsedValue)) {
+    throw new InvalidArgumentError("Not a number.");
+  }
+  return parsedValue;
+};
+
+program
+  .name("node-rdap")
+  .description("CLI for querying rdap via nodejs")
+  .version("0.3.0");
+
+program.exitOverride();
+
+program
+  .command("domain")
+  .description("Lookup a domains whois info via RDAP")
+  .argument("<domain>", "The domain to lookup")
+  .action(async (arg: string) => doCommand(domain, arg));
+
+program
+  .command("ip")
+  .description("Lookup a domains whois info via RDAP")
+  .argument("<ip>", "The IP Address to lookup")
+  .action(async (arg: string) => doCommand(ip, arg));
+
+program
+  .command("autnum")
+  .description("Lookup a domains whois info via RDAP")
+  .argument("<autnum>", "The Autonomous System Number to lookup", parseArgInt)
+  .action(async (arg: number) => doCommand(autnum, arg));
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: true,
+});
+
+rl.on("line", (line) => {
+  const commands = line.split(" ");
+  try {
+    program.parse(commands, { from: "user" });
+  } catch (e) {
+    // Show the help info if command doesn't exist.
+    console.log(program.helpInformation());
+  }
+});
